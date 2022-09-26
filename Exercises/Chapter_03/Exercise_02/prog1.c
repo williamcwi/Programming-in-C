e011e00860fa8a559c376e1d35503403bd28b88b
William <williamcwi.chu@hotmail.com>
2022-09-26 15:16:10 +0100

    Added exercise 2

---
+++ Exercises/Chapter_03/Exercise_02/prog1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf ("1. In C, lowercase letters are significant. \n2. main is where program execution begins. \n3. Opening and closing braces enclose programstatements in a routine. \n4. All program statements must be terminated by a semicolon. ");
+
+    return 0;
+}
